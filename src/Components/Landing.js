39dc395c6aacd8cd7c875ec19cef3a3b16957dad
Kesar <67405604+SoloSaber@users.noreply.github.com>
2021-10-24 05:51:03 +0100

    updates

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -9,7 +9,7 @@ export function Landing () {
                         <div id='app' style={{borderStyle: "none", paddingTop: '10%'}}>
                             <h1 style={{ padding: '10px', fontFamily: 'Inter', fontWeight: '700', fontSize: '6vw' }}>Connect 🔌 your wallet and find out 🔮 your NFTPASS score! 💎</h1>
                             <Row style={{ padding: '10px', paddingTop: '5%'}}>
-                                <Col xs={{span: 12, order: 1}} lg={{span: 'auto'}} style={{paddingTop: '10px'}}>
+                                <Col xs={{span: 'auto'}} style={{paddingTop: '10px'}}>
                                     <Button className='border-0 w-100' style={{borderRadius: '0rem', backgroundColor: 'rgb(0,0,0)', color: 'white', fontFamily: 'Inter', fontWeight: '700', padding: '10px 20px 10px 20px'}} href='/score'>Get your Score ↗</Button>
                                 </Col>
                             </Row>
