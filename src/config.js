dc352e50a46ea32405f7098b1473ef37c874e25d
Kesar <cesar.huret@gmail.com>
2021-10-26 23:52:04 +0100

    update contract address

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
     backend_endpoint: process.env.REACT_APP_BACKEND_URL || 'https://nftpass.herokuapp.com',
     fontend_endpoint: process.env.REACT_APP_FRONTEND_URL || 'https://nftpass.xyz',
-    contract_address: process.env.REACT_APP_CONTRACT_ADDRESS || '0x48647b5E64f4ECb7F9E2BA11461Cc2fA4438d816',
+    contract_address: process.env.REACT_APP_CONTRACT_ADDRESS || '0xFdD8B67c0E63e93Aa1963248646378a3E8C819f4',
     google_analytics: process.env.REACT_APP_GOOGLE_ANALYTICS || "G-TWE1F7Y03G"
 }
 
